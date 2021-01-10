f91ddd889aa0c4af9dd8d36006b8edbd6d3f2087
henry sanchez <hrsd83@gmail.com>
2021-01-10 11:18:23 -0300

    Se crea la funcion para validar los campos del formulario

--- form.js
+++ form.js
@@ -6,6 +6,8 @@ let alldata = []
 
 // Se crea la funtion para guardar datos del formulario
 function saveData(){
+  // Validamos
+  validate()
   // array de datos
   data= [];
 
@@ -25,7 +27,48 @@ function saveData(){
     
   // Almacenamos los datos en la BD Local
     localStorage.setItem('frm',arjson ) 
-   
+}
 
+
+// Validar todos los campos
+function validate(){
+  // todos los campos
+  let name = document.getElementById('name').value;
+  let lastName = document.getElementById('last_name').value;
+  let mail = document.getElementById('mail').value;
+  let countries = document.getElementById('countries').value;
+  let phone = document.getElementById('phone').value;
+  let work = document.getElementById('work').value;
+
+  // Validamos el nombre
+  if(name.length < 2){
+    alert('Falta el nombre');
+    return false;
+  }
+  // Validamos el apellido
+  if(lastName.length < 2){
+    alert('Falta el apellido');
+    return false;
+  }
+  // Validamos el email
+  if(mail.length < 10){
+    alert('Falta el email');
+    return false;
+  }
+  // Validamos el pais
+  if(paises.length < 5){
+    alert('Falta el pais');
+    return false;
+  }
+  // Validamos el telefono
+  if(phone.length < 1){
+    alert('Falta el pais');
+    return false;
+  }
+    // Validamos el trabajo
+    if(work.length < 1){
+      alert('Falta el puesto de trabajo');
+      return true
+    }
 
 }
